1bba4c913c102362a18282b95eeac0816c8f5287
itech001 <itech001@126.com>
2016-06-30 08:36:44 +0100

    next execution, config history

--- jenkins.sh
+++ jenkins.sh
@@ -4,4 +4,4 @@ scriptdir=`dirname "$BASH_SOURCE"`
 export JENKINS_ROOT=$scriptdir
 export JENKINS_HOME=$JENKINS_ROOT/jenkins_home
 echo JENKINS_HOME:$JENKINS_HOME 
-java -jar $JENKINS_ROOT/jenkins.war --httpPort=8008
+java -Dorg.apache.commons.jelly.tags.fmt.timeZone=Asia/Shanghai -jar $JENKINS_ROOT/jenkins.war --httpPort=8008
